083f2c7ec89a6174b06999763209d05fb0031ccb
tamar0583235240 <tamar0583235240@gmail.com>
2025-06-25 23:34:10 +0300

    Initial project setup: frontend + backend

---
+++ backend/src/config/corsOptions.js
@@ -0,0 +1,10 @@
+var corsOptions = {
+  origin: [
+    'http://localhost:5173'
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}
+
+module.exports = corsOptions;
