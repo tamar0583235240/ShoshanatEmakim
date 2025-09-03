62e11795416b4b86898109994ed5621bdbe4f32d
ester-meyer <mindy05832@gmail.com>
2025-09-03 20:10:32 +0300

    Get products in SubCategory Page

--- frontend/src/pages/SubCategoryPage.tsx
+++ frontend/src/pages/SubCategoryPage.tsx
@@ -1,13 +1,27 @@
-import { useParams } from "react-router-dom";
+import { Outlet, useParams } from "react-router-dom";
 import type { SubCategoryPageProps } from "../features/products/types/SubCategoryPageProps";
+import { getData } from "../service/apiService";
+import { useEffect, useState } from "react";
 
 const SubCategoryPage: React.FC<SubCategoryPageProps> = () => {
 
-    const { category, subCategory } = useParams<SubCategoryPageProps>();
+  const { subCategory } = useParams<{ subCategory: string }>();
+  const [product, setProduct] = useState<string>("");
+
+  const getProducts = async () => {
+    console.log("in product page ", subCategory)
+    setProduct(await getData(`/product/getByCategory/${subCategory}`))
+    console.log("product: ", product)
+  }
+  useEffect(() => {
+    getProducts()
+  }, [])
+
+  console.log(`Rendering CategoryPage for category: ${subCategory}`);
   return (
     <div>
-      <h1>קטגוריה: {category}</h1>
-      <h2>תת־קטגוריה: {subCategory}</h2>
+      <h1>קטגוריה: {subCategory}</h1>
+      <Outlet />
     </div>
   );
 };
