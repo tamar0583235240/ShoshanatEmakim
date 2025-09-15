ebccc6a49c5f8f1868a81a2408e21954f06eda28
ester-meyer <mindy05832@gmail.com>
2025-09-15 17:51:08 +0300

    Clear - delete not used pages

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,9 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import AppLayout from "./layouts/AppLayout";
 import HomePage from "./pages/HomePage";
-import AboutPage from "./pages/AboutPage";
 import NotFound from "./pages/NotFound";
-import AdminPage from "./pages/AdminPage";
 import CategoryPage from "./pages/CategoryPage";
 import SubCategoryPage from "./pages/SubCategoryPage";
 import ProductForm from "./pages/AddProduct";
@@ -14,9 +12,6 @@ const router = createBrowserRouter([
     element: <AppLayout />,
     children: [
       { index: true, element: <HomePage /> },
-      { path: 'about', element: <AboutPage /> },
-      { path: 'admin', element: <AdminPage /> },
-      { path: 'add', element: <ProductForm /> },
       {
         path: ':category',
         children: [
