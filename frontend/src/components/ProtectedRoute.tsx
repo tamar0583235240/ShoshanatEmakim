b22b7fbb2b1db6f0eb5f11aa013db8b23e0c6f99
ester-meyer <mindy05832@gmail.com>
2025-09-19 01:18:42 +0300

    merge

---
+++ frontend/src/components/ProtectedRoute.tsx
@@ -0,0 +1,17 @@
+import { Navigate } from "react-router-dom";
+import type { JSX } from "react/jsx-dev-runtime";
+import { useAdmin } from "../context/AdminContext";
+
+type Props = {
+  children: JSX.Element;
+};
+
+const ProtectedRoute = ({ children }: Props) => {
+  const { isLoggedIn } = useAdmin();
+  if (!isLoggedIn) {
+    return <Navigate to="/admin/login" replace />;
+  }
+  return children;
+};
+
+export default ProtectedRoute;
