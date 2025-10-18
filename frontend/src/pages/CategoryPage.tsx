68bb85365b85d3679bd9481b1eebc5c1ffd16902
tamar0583235240 <tamarb240@gmail.com>
2025-10-18 20:04:10 +0300

    Category page design

--- frontend/src/pages/CategoryPage.tsx
+++ frontend/src/pages/CategoryPage.tsx
@@ -1,12 +1,17 @@
 import { Outlet, useParams } from "react-router-dom";
 import type { CategoryPageProps } from "../features/products/types/CategoryPageProps";
+import "../style/CategoryPage.css"
 
 const CategoryPage: React.FC = () => {
   const { category } = useParams<CategoryPageProps>();
   console.log(`Rendering CategoryPage for category: ${category}`);
   return (
-    <div>
-      <h1>קטגוריה: {category}</h1>
+    <div className="category-page">
+      <div className="breadcrumb">
+        עמוד הבית <span>›</span> {category}
+      </div>
+      <h1 className="category-title">{category}</h1>
+      <div className="title-underline"></div>
       <Outlet />
     </div>
   );
