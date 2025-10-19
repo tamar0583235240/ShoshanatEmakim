c690e2a30a801c388a8d9fa9ad1efc142ea185c9
tamar0583235240 <tamarb240@gmail.com>
2025-10-20 02:03:17 +0300

    change cors

--- backend/src/config/corsOptions.js
+++ backend/src/config/corsOptions.js
@@ -2,7 +2,8 @@ var corsOptions = {
   origin: [
     process.env.FRONTEND_URL,
     "https://shoshanat-front.vercel.app",
-    "http://localhost:3000"
+    "http://localhost:3000",
+    "http://localhost:5173"
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
